543dd63b9510c5aec9ba195c1325a67259180198
niyatiUnhex <84308236+niyatiUnhex@users.noreply.github.com>
2021-06-05 23:45:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className = "note" >
+      <h1>Javascript and React.js</h1>
+      <p>This was an amazing bootcamp organised by ShapeAI, by shaurya sinha.
+      We covered everything from scratch.
+      React.js, HTMl.</p>
+    </div>
+
+  );
+
+}
+export default Note;
